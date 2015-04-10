168ab0edbbd9f652cc451f388b8c78e833b990cd
Guillermo Saenz <g42v@me.com>
2015-04-09 20:59:30 -0500

    Initial Commit

---
+++ CollectionViewPrueba/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewPrueba
+//
+//  Created by Guillermo Saenz on 4/9/15.
+//  Copyright (c) 2015 Property Atomic Strong SAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionViewPrueba/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewPrueba
+//
+//  Created by Guillermo Saenz on 4/9/15.
+//  Copyright (c) 2015 Property Atomic Strong SAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
